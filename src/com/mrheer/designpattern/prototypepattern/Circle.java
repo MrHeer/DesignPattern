6d067cd8b3d4ca899567224515951118ac7ef326
MrHeer <hlm52pk@163.com>
2018-05-28 15:49:20 +0800

    add prototype pattern and README

---
+++ src/com/mrheer/designpattern/prototypepattern/Circle.java
@@ -0,0 +1,12 @@
+package com.mrheer.designpattern.prototypepattern;
+
+public class Circle extends Shape {
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/com/mrheer/designpattern/prototypepattern/Shape.java
@@ -0,0 +1,35 @@
+package com.mrheer.designpattern.prototypepattern;
+
+public abstract class Shape implements Cloneable {
+    protected String type;
+    private String id;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    protected Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
