def48a806c2faf9c6aac71c29ebccd928b1e9aca
MrHeer <hlm52pk@163.com>
2019-04-25 21:23:30 +0800

    add state pattern

---
+++ src/com/mrheer/designpattern/statepattern/StartState.java
@@ -0,0 +1,13 @@
+package com.mrheer.designpattern.statepattern;
+
+public class StartState implements State {
+
+    public void doAction(Context context) {
+        System.out.println("Player is in start state");
+        context.setState(this);
+    }
+
+    public String toString() {
+        return "Start State";
+    }
+}
---
+++ src/com/mrheer/designpattern/statepattern/State.java
@@ -0,0 +1,5 @@
+package com.mrheer.designpattern.statepattern;
+
+public interface State {
+    public void doAction(Context context);
+}
