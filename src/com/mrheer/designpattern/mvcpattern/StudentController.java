917eff0778b19f70794d64584040fadbdaebf2db
MrHeer <hlm52pk@163.com>
2019-05-11 20:41:29 +0800

    add mvc pattern

---
+++ src/com/mrheer/designpattern/mvcpattern/StudentController.java
@@ -0,0 +1,31 @@
+package com.mrheer.designpattern.mvcpattern;
+
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public String getStudentName() {
+        return model.getName();
+    }
+
+    public void setStudentName(String name) {
+        model.setName(name);
+    }
+
+    public String getStudentRollNo() {
+        return model.getRollNo();
+    }
+
+    public void setStudentRollNo(String rollNo) {
+        model.setRollNo(rollNo);
+    }
+
+    public void updateView() {
+        view.printStudentDetails(model.getName(), model.getRollNo());
+    }
+}
