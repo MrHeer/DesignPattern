b4d6874266c51359b836a435a5e5ca8f522ca67d
MrHeer <hlm52pk@163.com>
2019-05-13 22:22:57 +0800

    add composite entity pattern

---
+++ src/com/mrheer/designpattern/compositeentitypattern/DependentObject1.java
@@ -0,0 +1,14 @@
+package com.mrheer.designpattern.compositeentitypattern;
+
+public class DependentObject1 {
+
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ src/com/mrheer/designpattern/compositeentitypattern/DependentObject2.java
@@ -0,0 +1,14 @@
+package com.mrheer.designpattern.compositeentitypattern;
+
+public class DependentObject2 {
+
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
