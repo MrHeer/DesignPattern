9ec0e45b5d02f7bdb3d359990a0f15b6d634d9c1
MrHeer <hlm52pk@163.com>
2019-05-17 22:24:28 +0800

    add front controller pattern

---
+++ src/com/mrheer/designpattern/frontcontrollerpattern/HomeView.java
@@ -0,0 +1,7 @@
+package com.mrheer.designpattern.frontcontrollerpattern;
+
+public class HomeView {
+    public void show() {
+        System.out.println("Displaying Home Page");
+    }
+}
---
+++ src/com/mrheer/designpattern/frontcontrollerpattern/StudentView.java
@@ -0,0 +1,7 @@
+package com.mrheer.designpattern.frontcontrollerpattern;
+
+public class StudentView {
+    public void show() {
+        System.out.println("Displaying Student Page");
+    }
+}
